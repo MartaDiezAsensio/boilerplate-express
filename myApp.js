386e1eb63ceb697372e2019a5bde9652738a6bba
Marta Diez Asensio <114190534+MartaDiezAsensio@users.noreply.github.com>
2023-10-31 18:36:18 +0100

    Code added to myApp.js

--- myApp.js
+++ myApp.js
@@ -1,26 +1,28 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 
 // Define a route for the root URL ("/")
-
-// function	handleRootReq(req, res) {
-// 	res.send('Hello Express');
-// }
-// app.get('/', handleRootReq);
-
 
 // app.get('/', (req, res) => {
 // 	res.send('Hello Express');
 // })
 
-//absolutePath = __dirname + '/views/index.html';
+
 
 app.get('/', (req, res) => {
+	app.use('/public', express.static(__dirname + '/public'));
 	res.sendFile(__dirname + '/views/index.html');
 });
 
-
+app.get('/json', (req, res) => {
+	if (process.env.MESSAGE_STYLE === "uppercase"){
+		res.json({"message": "HELLO JSON"});
+	}else {
+		res.json({"message": "Hello json"});
+	}
+})
 
 
 
